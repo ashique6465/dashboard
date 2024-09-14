637b5ccadd94852a62e73e82c76727058f0e77b3
ashique6465 <mdashiqueali71@gmail.com>
2024-09-15 02:57:10 +0530

    Deal and Events

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsCharts from "./home/dealscharts";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+
+export { 
+    UpcomingEvents, 
+    DealsCharts, 
+
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton
+
+}
